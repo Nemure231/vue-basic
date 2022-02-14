b6406ff1f09801878b08e054312b6974a02f2d99
Nemure231 <neruhi5321@gmail.com>
2022-02-14 18:21:03 +0700

    Meberi style

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,19 @@
 <template>
 
 
-<div>Web Belajar Vue Saya</div>
+<h1 class="title">Web Belajar Vue Saya</h1>
 <br><br>
 
 <div class="wrap-link">
-    <router-link to="/dec-render">Decrender</router-link> |
+    <router-link to="/dec-render">Decrender</router-link>
     <router-link to="/props">Props</router-link>
-
-    <router-link to="/method">Method</router-link> |
+    <router-link to="/method">Method</router-link>
     <router-link to="/model">Model</router-link>
-    <router-link to="/conditional">Conditional</router-link> |
+    <router-link to="/conditional">Conditional</router-link>
     <router-link to="/looping">Looping</router-link>
-    <router-link to="/lifecycle">Lifecycle</router-link> |
+    <router-link to="/lifecycle">Lifecycle</router-link>
     <router-link to="/computed">Computed</router-link>
-    <router-link to="/watcher">Watcher</router-link> |
+    <router-link to="/watcher">Watcher</router-link>
     <router-link to="/dynamic-class">Dynamic Class</router-link>
 </div>
 
@@ -44,6 +43,16 @@
   flex-wrap: wrap;
   justify-content: center;
   align-content: center;
+}
+
+a{
+  text-decoration-line: none;
+  font-weight: 600;
+  color:cornflowerblue;
+}
+
+.title{
+  color: rgb(82, 0, 123);
 }
 
 .wrap-link > :not([hidden]) ~ :not([hidden]) {
